07be16c186ea1dbf20aee70833990314cd524d09
Martyn Davies <martyn.davies@algolia.com>
2018-01-29 11:36:14 +0000

    remove dist

--- UI/src/components/Indexer.vue
+++ UI/src/components/Indexer.vue
@@ -178,7 +178,7 @@ export default {
         },
       });
       this.isLoading = false;
-      this.$store.dispatch('emptyState', []);
+      this.$store.dispatch('emptyState', {});
     },
 
     async reindex() {
