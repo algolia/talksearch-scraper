02a9ed03e6e974d607773b532ce9355038eb0cd8
Pixelastic <tim@pixelastic.com>
2018-05-31 16:53:10 +0200

    Clean up index script

--- scripts/index.js
+++ scripts/index.js
@@ -12,33 +12,10 @@ youtube.on('playlist:chunk', progress.youtube.onPlaylistChunk);
 youtube.on('playlist:end', progress.youtube.onPlaylistEnd);
 youtube.on('error', progress.onError);
 youtube.on('warning', progress.onWarning);
-// youtube.on('video:end', progress.youtube.onVideoEnd);
-// youtube.on('playlist:end', progress.youtube.onPlaylistEnd);
-// youtube.on('playlist:get:page', progress.onPlaylistGetPage);
-// youtube.on('playlist:get:end', progress.onPlaylistGetEnd);
-// youtube.on('video:data:start', progress.onVideoDataStart);
-// youtube.on('video:data:basic', progress.onVideoDataBasic);
-// youtube.on('video:data:end', progress.onVideoDataEnd);
-// youtube.on('video:captions:start', progress.onVideoCaptionsStart);
-// youtube.on('video:raw:start', progress.onVideoRawStart);
-// youtube.on('video:error', progress.onVideoError);
 
 algolia.on('batch:start', progress.algolia.onBatchStart);
 algolia.on('batch:chunk', progress.algolia.onBatchChunk);
 algolia.on('batch:end', progress.algolia.onBatchEnd);
-
-// algolia.on('remoteObjectIds:start', progress.algolia.onPushBefore);
-// algolia.on('push:before', progress.algolia.onPushBefore);
-// algolia.on('push:chunk', progress.algolia.onPushChunk);
-// algolia.on('settings:before', () => {
-//   console.info('Pushing settings');
-// });
-// algolia.on('overwrite:before', () => {
-//   console.info('Overwriting index');
-// });
-// algolia.on('overwrite:after', () => {
-//   console.info('✔ Done');
-// });
 
 /**
  * Parsing command line arguments
@@ -68,13 +45,11 @@ const argv = yargs
     const videos = await youtube.getVideos();
     progress.displayWarnings();
 
-    // // Transform videos in records
-    // const records = transformer.run(videos);
-    // console.info(videos);
-    // console.info(`${records.length} records generated`);
+    // Transform videos in records
+    const records = transformer.run(videos);
 
-    // // Push records
-    // await algolia.run(records);
+    // Push records
+    await algolia.run(records);
   } catch (err) {
     console.info(err);
   }
