cc3e81c52bd4ba581883a1b64c7658d39feaf64f
Pixelastic <tim@pixelastic.com>
2018-08-10 09:17:13 +0200

    Adding Critical Role

---
+++ configs/criticalrole.js
@@ -0,0 +1,7 @@
+export default {
+  indexName: 'criticalrole',
+  playlists: [
+    'PL1tiwbzkOjQz7D0l_eLJGAISVtcL7oRu_', // Campaign 1: Vox Machina
+    'PL1tiwbzkOjQxD0jjAE7PsWoaCrs0EkBH2', // Campaign 2: The Mighty Nein
+  ],
+};
