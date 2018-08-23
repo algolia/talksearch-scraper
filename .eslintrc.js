0c01451e6dbcbc018acc5769525193eaf7b5c939
Pixelastic <tim@pixelastic.com>
2018-08-23 21:01:53 +0200

    test(config): Remove unused eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   extends: ['algolia', 'algolia/jest'],
-  globals: {
-    __rewire_reset_all__: true,
-  },
   rules: {
     'no-console': 0,
     'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
